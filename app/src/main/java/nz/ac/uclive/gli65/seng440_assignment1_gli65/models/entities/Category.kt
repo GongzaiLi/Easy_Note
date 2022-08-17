158bfa4eabf46284ed0c346e53fab8226946f86d
Gongzai Li <gli65@uclive.ac.nz>
2022-08-17 18:19:09 +1200

    feat: create the care in navigation drawer and make Screen can support both landscape and portrait

---
+++ app/src/main/java/nz/ac/uclive/gli65/seng440_assignment1_gli65/models/entities/Category.kt
@@ -0,0 +1,12 @@
+package nz.ac.uclive.gli65.seng440_assignment1_gli65.models.entities
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import java.io.FileDescriptor
+
+class Category(
+    val title: String,
+    val contentDescriptor: String,
+    val icon: String,
+) {
+
+}
