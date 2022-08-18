3f5e6707108ec38914f73570b33e2e26c671d52d
Gongzai Li <gli65@uclive.ac.nz>
2022-08-18 22:16:29 +1200

    add: add some new color

--- app/src/main/java/nz/ac/uclive/gli65/seng440_assignment1_gli65/ui/theme/Color.kt
+++ app/src/main/java/nz/ac/uclive/gli65/seng440_assignment1_gli65/ui/theme/Color.kt
@@ -12,3 +12,12 @@ val WhiteSmoke = Color(0xFFF5F5F5)
 val TextWhite = Color(0xFFEEEEEE)
 val AquaBlue = Color(0xFF9AA5C4)
 val LightRed = Color(0xfffc879a)
+
+val DarkGray = Color(0xFF202020)
+val LightBlue = Color(0xFFD7E8DE)
+
+val RedOrange = Color(0xFFFFab91)
+val RedPink = Color(0xFFF48fb1)
+val BabyBlue = Color(0xFF81DEEA)
+val Violet = Color(0xFFCF94DA)
+val LightGreen = Color(0xFFE7ED9B)
