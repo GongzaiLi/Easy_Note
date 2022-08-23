775e9680f8c56368219501d55cfecbe186036d1b
Gongzai Li <gli65@uclive.ac.nz>
2022-08-23 20:49:24 +1200

    done: </done>

--- app/src/main/java/nz/ac/uclive/gli65/seng440_assignment1_gli65/views/component/Notification.kt
+++ app/src/main/java/nz/ac/uclive/gli65/seng440_assignment1_gli65/views/component/Notification.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.platform.LocalContext
 import nz.ac.uclive.gli65.seng440_assignment1_gli65.R
 
 
-fun titleNotification(context: Context, title: String) : Boolean{
+fun titleNotification(context: Context, title: String): Boolean {
 
 
     val isTitleNull = title.isBlank()
@@ -15,7 +15,6 @@ fun titleNotification(context: Context, title: String) : Boolean{
     val message: String =
         if (isTitleNull)
             context.resources.getString(R.string.null_title_notification)
-
         else
             String.format(context.resources.getString(R.string.save_toast_message), title)
 
